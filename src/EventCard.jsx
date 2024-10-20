416d9cd0b6df7d2f53837e907540afc788f5155b
Gabriely Carrari <gabymcarrari@gmail.com>
2024-10-20 14:08:47 -0300

    ♻️ added details on event card

--- src/EventCard.jsx
+++ src/EventCard.jsx
@@ -6,6 +6,17 @@ const EventCard = () => {
                 <div className="card-body">
                     <h5 className="card-title">Título Evento</h5>
                     <p className="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum quia inventore laudantium placeat necessitatibus beatae ducimus delectus nemo quod, sequi a earum tempora assumenda corrupti!</p>
+                    <div className="d-flex flex-column">
+                        <p className="card-text mb-1" >
+                            <i className="bi bi-calendar2-event-fill pe-2"></i>24/10/2024
+                            <i className="bi bi-clock-fill pe-2 ps-4"></i>13:00
+                            
+                        </p>
+                        <p className="card-text">
+                            <i className="bi bi-hourglass-split pe-2"></i>Carga Horária: 2h
+                        </p>
+                    </div>
+
                     <div className="d-flex justify-content-end">
                         <a href="#" className="btn btn-outline-primary text-end">Ver Participantes</a>
                     </div>
