069fede77f6842e9643da78be9aa75502248fb3f
Gabriely Carrari <gabymcarrari@gmail.com>
2024-12-04 21:57:31 -0300

    ✨ add registro presença

--- src/ModalRegisterAttendance.jsx
+++ src/ModalRegisterAttendance.jsx
@@ -1,11 +1,57 @@
 import React, { useState } from 'react';
+import {DateFormatter, StringFormatter } from './js/formatters';
+import { getUsuario } from './js/authService';
+import api from "./js/axiosApi";
 
-const ModalRegisterAttendance = () => {
+const ModalRegisterAttendance = ({ evento }) => {
     const [isChecked, setIsChecked] = useState(false); 
 
     const handleCheckboxChange = (event) => {
         setIsChecked(event.target.checked);
     };
+
+
+    const handleConfirmAttendance = async () => {;
+        const user = getUsuario();
+
+        if (user === null) {
+            navigate("/login");
+            return;
+        }
+        if (user.perfil !== 1) {
+            //TODO: disparar erro
+            navigate("/");
+            return;
+        }
+        await api.post("/registrar_presenca", { 
+            "id_participante": user.id,
+            "id_evento": evento.id
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                if (response.data) {
+                    // TODO: Adicionar feedback de sucesso
+                    console.log("Presença registrada com sucesso");
+                    navigate("/my-attendances");
+                }
+            } else {
+                //TODO: Adicionar feedback de erro
+                console.log("Error: " + response);
+            }
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+    }
+
+    let now = new Date();
+    let data_inicio = '';
+
+    if (evento) {
+        data_inicio = new Date(`${evento.data_inicio}T${evento.hora_inicio}:00`);
+        let data_fim = new Date(data_inicio); 
+        data_fim.setHours(data_inicio.getHours() + evento.carga_horaria); 
+    }
 
     return (
         <div className="modal fade" id="modalRegisterAttendance" data-bs-backdrop="static" data-bs-keyboard="true" tabIndex="-1">
@@ -16,19 +62,19 @@ const ModalRegisterAttendance = () => {
                         <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
                     </div>
                     <div className="modal-body">
-                        <h5>Título Evento</h5>
+                        <h5>{evento ? StringFormatter.Capitalize(evento.nome) : ''}</h5>
 
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum quia inventore laudantium placeat necessitatibus beatae ducimus delectus nemo quod, sequi a earum tempora assumenda corrupti!</p>
+                        <p>{evento?.descricao}</p>
 
                         <p className="">
-                            <strong>Carga Horária:</strong> 2 horas
+                            <strong>Carga Horária:</strong> {evento?.carga_horaria} horas
                         </p>
                         <div className="d-flex flex-column ">
                             <p className=" mb-1">
-                                <i className="bi bi-calendar2-event-fill pe-2"></i>24/10/2024
+                                <i className="bi bi-calendar2-event-fill pe-2"></i>{evento ? DateFormatter.format(data_inicio) : ''}
                             </p>
                             <p className="">
-                                <i className="bi bi-clock-fill pe-2"></i>13:00
+                                <i className="bi bi-clock-fill pe-2"></i>{evento?.hora_inicio}
                             </p>
                         </div>
                         <div className="form-check small fst-italic">
@@ -40,7 +86,7 @@ const ModalRegisterAttendance = () => {
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-outline-danger" data-bs-dismiss="modal">Cancelar</button>
-                        <button type="button" className="btn btn-success" data-bs-dismiss="modal" disabled={!isChecked}>Confirmar</button>
+                        <button type="button" className="btn btn-success" data-bs-dismiss="modal" disabled={!isChecked} onClick={handleConfirmAttendance}>Confirmar</button>
                     </div>
                 </div>
             </div>
--- src/MyAttendances.jsx
+++ src/MyAttendances.jsx
@@ -1,11 +1,38 @@
 import ListAttendancesCards from './ListAttendancesCards';
 import ModalRegisterAttendance from './ModalRegisterAttendance';
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import api from './js/axiosApi';
 
 const MyAttendances = () => {
+    const [evento, setEvento] = useState(null);
+    const [eventKey, setEventKey] = useState('');  
+
+    const handleFindEvent = (eventKey) => {
+        console.log('Buscando evento com chave:', eventKey);
+        console.log(eventKey);
+        api.get(`obter_evento_por_chave/${eventKey}`)
+            .then((response) => {
+                setEvento(response.data);
+                console.log(response.data);
+                handleRegisterAttendance();
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                // setLoading(false);
+            });
+    }
+
     const handleRegisterAttendance = () => {
         const modal = new bootstrap.Modal(document.getElementById('modalRegisterAttendance'));
         modal.show();
+    }
+
+    const handleInputChange = (e) => {
+        console.log(e.target.value);
+        setEventKey(e.target.value);
     }
 
     return (
@@ -16,8 +43,8 @@ const MyAttendances = () => {
             <div className="d-flex mt-5">
                 <form action="" className="w-50">
                     <div className="input-group" >
-                        <input type="text" className="form-control" placeholder="Identificador do Evento" />
-                        <button className="btn btn-outline-primary" type="button" id="idCarteira" onClick={() => handleRegisterAttendance()}>
+                        <input type="text" className="form-control" id="eventKey" name="eventKey" placeholder="Identificador do Evento"  value={eventKey} onChange={handleInputChange}/>
+                        <button className="btn btn-outline-primary" type='button' onClick={() => handleFindEvent(eventKey)}>
                             <i className="bi bi-person-add pe-2"></i>
                             Registrar Presença
                         </button>
@@ -26,7 +53,9 @@ const MyAttendances = () => {
             </div>
        
             <ListAttendancesCards />
-            <ModalRegisterAttendance />
+            
+            <ModalRegisterAttendance evento={evento}/>
+            
         </>
     );
 }
